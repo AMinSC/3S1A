2a75d191ccefa94888c3e05e5969ebc7410ece8a
AMinSC <cdtjdcjf@gmail.com>
2023-08-09 11:56:27 +0000

    GPT 통신 postman으로 확인

--- config/chatbot/views.py
+++ config/chatbot/views.py
@@ -36,35 +36,35 @@ class ChatbotView(APIView):
 
             prompt_with_previous = f"{previous_conversations_str}\n{role}: {prompt}\nAI:"
 
-            model_engine = "text-davinci-003"
-            completions = openai.Completion.create(
-                # OpenAI 모델
-                engine=model_engine,
-                # 현재 사용자의 입력과 이전 대화 기록을 모두 포함
-                prompt=prompt_with_previous,
-                # 응답의 최대 길이
-                max_tokens=1024,
-                # 응답 개수
-                n=5,
-                # API가 추가 토큰 생성을 중지해야 하는 일련의 시퀀스를 지정하는 선택적 매개변수(지정하지 않을 경우 모델이 정지하기에 가장 좋은 위치를 결정)
-                stop=None,
-                # 응답의 무작위성 값이 낮을수록 출력에 집중하며, 높을수록 무작위 답변
-                temperature=0.5,
-            )
-            response = completions.choices[0].text.strip()
+        model_engine = "text-davinci-003"
+        completions = openai.Completion.create(
+            # OpenAI 모델
+            engine=model_engine,
+            # 현재 사용자의 입력과 이전 대화 기록을 모두 포함
+            prompt=prompt_with_previous,
+            # 응답의 최대 길이
+            max_tokens=2048,
+            # 응답 개수
+            n=5,
+            # API가 추가 토큰 생성을 중지해야 하는 일련의 시퀀스를 지정하는 선택적 매개변수(지정하지 않을 경우 모델이 정지하기에 가장 좋은 위치를 결정)
+            stop=None,
+            # 응답의 무작위성 값이 낮을수록 출력에 집중하며, 높을수록 무작위 답변
+            temperature=0.5,
+        )
+        response = completions.choices[0].text.strip()
 
-            conversation_serializer = MessageSerializer(data={'user': user.id, 'role':role, 'prompt': prompt})
+        conversation_serializer = MessageSerializer(data={'user': user.id, 'role':role, 'prompt': prompt})
 
-            if conversation_serializer.is_valid():
-                conversation_serializer.save()
-            else:
-                return Response(conversation_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if conversation_serializer.is_valid():
+            conversation_serializer.save()
+        else:
+            return Response(conversation_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            # 대화 기록에 새로운 응답 추가
-            conversations_data.append({'role': 'AI', 'prompt': response})
+        # 대화 기록에 새로운 응답 추가
+        conversations_data.append({'role': 'AI', 'prompt': response})
 
-            request.session['conversations'] = conversations_data
-            request.session.modified = True
+        request.session['conversations'] = conversations_data
+        request.session.modified = True
             
         return Response({'conversation': conversations_data})
 
