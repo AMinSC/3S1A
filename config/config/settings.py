b3bb5601e1bd96860787bccd8cd6f3a77d362228
AMinSC <cdtjdcjf@gmail.com>
2023-08-10 08:54:47 +0000

    DRF static 설정중

--- config/config/settings.py
+++ config/config/settings.py
@@ -188,7 +188,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = '/srv/S3A1/config/static'
 STATIC_URL = 'static/'
+
+# STATICFILES_DIRS = (
+#        os.path.join(BASE_DIR, 'static'),
+#        )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
